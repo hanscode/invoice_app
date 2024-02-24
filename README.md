a3fec498e1a6a3b3e1e38017c20b447759aed06a
Hans Steffens <hans@steffens.co>
2024-02-23 19:49:59 -0500

    Set up the API invoices routes

---
+++ README.md
@@ -0,0 +1,35 @@
+# Satoshi Invoice
+### The most amazing and simplest invoice system for real people
+Author: The folks behind <a href="https://pricode.io/" target="_blank">Pricode</a>
+Contributors:
+Hans Steffens: <a href="https://www.linkedin.com/in/hanscode/" target="_blank">LinkedIn</a>
+Alejandro Giraldo: <a href="https://www.linkedin.com/in/alejandro-giraldo-63146ba1/" target="_blank">LinkedIn</a>
+
+## Project Overview
+Satoshi is a lightweight invoicing application strategically designed to help freelance professionals with their daily bills. Satoshi aims to make the billing process intuitive and simple enough. Carrying out the invoicing, specially for freelancers can be very annoying and tedious. Satoshi is desgined to facilitate this task which can be quite complex and time consuming.
+
+
+## Technologies used
+HTML, CSS, JavaScript, Node.js, Express, Sequelize, React, JSX, React Router, React Context API, Fetch API and Vite.js
+
+## 🛠 Installation & Set Up
+
+Our project contains a `.nvmrc` file containing a node version number, so anyone who clones the project can simply run 
+`nvm install` to sync up their node version.
+
+1. Install dependencies
+
+   ```sh
+   npm install
+   ```
+
+2. Start the development server
+
+   ```sh
+   npm start
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+>## License
+<a href="https://choosealicense.com/licenses/mit/" target="_blank">MIT</a>
--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const express = require("express");
 const { asyncHandler } = require("./middleware/async-handler");
 const User = require("./models").User;
 const Invoice = require("./models").Invoice;
-const { authenticateUser } = require('./middleware/auth-user');
+const { authenticateUser } = require("./middleware/auth-user");
 
 // Construct a router instance.
 const router = express.Router();
@@ -13,26 +13,28 @@ const router = express.Router();
  * Users Routes
  */
 
-// Route that returns all properties and values for the currently authenticated User 
+// Route that returns all properties and values for the currently authenticated User
 // along with a 200 HTTP status code.
-router.get("/users", authenticateUser, asyncHandler(async (req, res) => {
+router.get(
+  "/users",
+  authenticateUser,
+  asyncHandler(async (req, res) => {
     const authenticatedUser = req.currentUser;
     const userProperties = await User.findOne({
-      attributes: ['id', 'firstName', 'lastName', 'emailAddress'],
-      where: { id: authenticatedUser.id }
+      attributes: ["id", "firstName", "lastName", "emailAddress"],
+      where: { id: authenticatedUser.id },
     });
     res.status(200).json(userProperties);
-    })
-  );
+  })
+);
 
 // Route that creates a new user.
-router.post("/users", asyncHandler(async (req, res) => {
+router.post(
+  "/users",
+  asyncHandler(async (req, res) => {
     try {
       await User.create(req.body);
-      res
-        .status(201)
-        .setHeader("Location", "/")
-        .end();
+      res.status(201).setHeader("Location", "/").end();
     } catch (error) {
       console.log("ERROR: ", error);
 
@@ -49,5 +51,168 @@ router.post("/users", asyncHandler(async (req, res) => {
   })
 );
 
+/**
+ * Invoices Routes
+ */
+
+// Send a GET request to /invoices to return all invoices for the currently authenticated user.
+router.get(
+  "/invoices", authenticateUser, asyncHandler(async (req, res) => {
+    const authenticatedUser = req.currentUser;
+    const invoices = await Invoice.findAll({
+      attributes: [
+        "id",
+        "invoiceNumber",
+        "customerName",
+        "issueDate",
+        "dueDate",
+        "totalAmount",
+        "items",
+        "tax",
+        "discount",
+        "status",
+        "userId",
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "firstName", "lastName", "emailAddress"],
+        },
+      ],
+      where: { userId: authenticatedUser.id }
+    });
+    res.status(200).json(invoices);
+  })
+);
+
+// GET individual invoice details
+router.get("/invoices/:id", authenticateUser, asyncHandler(async (req, res, next) => {
+    const invoice = await Invoice.findOne({
+        attributes: [
+            "id",
+            "invoiceNumber",
+            "customerName",
+            "issueDate",
+            "dueDate",
+            "totalAmount",
+            "items",
+            "tax",
+            "discount",
+            "status",
+            "userId",
+          ],
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'firstName', 'lastName', 'emailAddress']
+        },
+      ],
+      where: { id: req.params.id }
+    });
+    if (invoice) {
+      res.status(200).json(invoice);
+    } else {
+      res.status(404).json({ message: "Invoice not found" });
+    }
+  })
+);
+
+// POST route that will create a new invoice.
+router.post(
+  "/invoices",
+  authenticateUser,
+  asyncHandler(async (req, res) => {
+    try {
+      const invoice = await Invoice.create(req.body);
+      res
+        .status(201)
+        .setHeader("Location", `/invoices/${invoice.id}`)
+        /** For clients to be able to access `Location` header,
+         * the server must list it using the HTTP `Access-Control-Expose-Headers` header. */
+        .setHeader("Access-Control-Expose-Headers", "Location")
+        .end();
+    } catch (error) {
+      console.log("Error: ", error);
+
+      // checking the error
+      if (
+        error.name === "SequelizeValidationError" ||
+        error.name === "SequelizeUniqueConstraintError"
+      ) {
+        const errors = error.errors.map((err) => err.message);
+        res.status(400).json({ errors });
+      } else {
+        // error caught in the asyncHandler's catch block
+        throw error;
+      }
+    }
+  })
+);
+
+// PUT route that will update the corresponding invoice and return a 204 HTTP status code and no content.
+router.put('/invoices/:id', authenticateUser, asyncHandler(async (req, res) => {
+    let invoice;
+    // retrieve the current authenticated user's information from the Request object's `currentUser` property:
+    const user = req.currentUser;
+    try {
+      invoice = await Invoice.findByPk(req.params.id);
+      if (invoice) {
+        // Confirming that the currently authenticated user is the owner of the requested invoice.
+        // This is because only the owner of the invoice should be able to update it. 
+        // In the future, we can add more roles and permissions to our application.
+        const invoiceOwner = invoice.userId;
+        const authenticatedUser = user.id;
+  
+        if (invoiceOwner === authenticatedUser) {
+            // update the invoice object from the request body
+            await invoice.update(req.body);
+            // Send status 204 (meaning no content == everything went OK but there's nothing to send back)
+            res.status(204).end();
+        } else {
+          res.status(403).json({message: "User is not owner of the requested invoice"});
+        }
+  
+      } else {
+        res.status(400).json({message: "Invoice not found"});
+      }
+    } catch (error) {
+      if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+        const errors = error.errors.map(err => err.message);
+        res.status(400).json({ errors });   
+      } else {
+        throw error;
+      }
+    }
+  
+  }))
+
+// DELETE route that will delete the corresponding invoice and return a 204 HTTP status code and no content.
+router.delete('/invoices/:id', authenticateUser, asyncHandler(async (req, res) => {
+    let invoice;
+    // retrieve the current authenticated user's information from the Request object's `currentUser` property:
+    const user = req.currentUser;
+    try {
+      invoice = await Invoice.findByPk(req.params.id);
+      if (invoice) {
+        // Confirming that the currently authenticated user is the owner of the requested invoice.
+        const invoiceOwner = invoice.userId;
+        const authenticatedUser = user.id;
+  
+        if (invoiceOwner === authenticatedUser) {
+          // Delete the invoice object
+          await invoice.destroy();
+          // Send status 204 (meaning no content == everything went OK but there's nothing to send back)
+          res.status(204).end();
+        } else {
+          res.status(403).json({message: "User is not owner of the requested invoice"});
+        }
+      } else {
+        res.status(400).json({message: "Invoice not found"});
+      }
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+  
+  }));
 
 module.exports = router;
