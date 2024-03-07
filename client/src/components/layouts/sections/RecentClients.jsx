26824edf5eee7918b21d52c553aa374da704e31a
Hans Steffens <hans@steffens.co>
2024-03-07 06:58:37 -0500

    Empty State for RecentClients Component

--- client/src/components/layouts/sections/RecentClients.jsx
+++ client/src/components/layouts/sections/RecentClients.jsx
@@ -8,6 +8,7 @@ import {
   FormatNumber,
 } from "../../../utils";
 
+import { PlusIcon } from "@heroicons/react/20/solid";
 import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { EllipsisHorizontalIcon } from "@heroicons/react/20/solid";
@@ -93,6 +94,47 @@ const RecentClients = () => {
 
   return (
     <>
+    {clients.length === 0 ? (
+      <div className="mt-8 flow-root mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="text-center py-12 mt-6 border rounded-lg">
+        <svg
+          className="mx-auto h-12 w-12 text-gray-400"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 48 48"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M34 40h10v-4a6 6 0 00-10.712-3.714M34 40H14m20 0v-4a9.971 9.971 0 00-.712-3.714M14 40H4v-4a6 6 0 0110.713-3.714M14 40v-4c0-1.313.253-2.566.713-3.714m0 0A10.003 10.003 0 0124 26c4.21 0 7.813 2.602 9.288 6.286M30 14a6 6 0 11-12 0 6 6 0 0112 0zm12 6a4 4 0 11-8 0 4 4 0 018 0zm-28 0a4 4 0 11-8 0 4 4 0 018 0z"
+          />
+        </svg>
+
+        <h2 className="mt-2 text-base font-semibold leading-6 text-gray-900">
+          Add clients
+        </h2>
+        <p className="mt-1 text-sm text-gray-500 text-center">
+          You haven&apos;t added any client to your account yet. <br />
+          Get started by creating a new client.
+        </p>
+        <div className="mt-6">
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            <PlusIcon
+              className="-ml-0.5 mr-1.5 h-5 w-5"
+              aria-hidden="true"
+            />
+            Add client
+          </button>
+        </div>
+      </div>
+    </div>
+    ) : (
+      <>
       <div className="flex items-center justify-between">
         <h2 className="text-base font-semibold leading-7 text-gray-900">
           Recent clients
@@ -209,6 +251,8 @@ const RecentClients = () => {
           </li>
         ))}
       </ul>
+      </>
+    )}
     </>
   );
 };
