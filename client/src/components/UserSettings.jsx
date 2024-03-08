e2464eafb81d54f94914914fe3bf10de27fc3fc0
Hans Steffens <hans@steffens.co>
2024-03-08 15:59:37 -0500

    Add a notification panel for successfully updated state

--- client/src/components/UserSettings.jsx
+++ client/src/components/UserSettings.jsx
@@ -1,7 +1,11 @@
 import { api } from "../utils/apiHelper";
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState, Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 import ErrorsDisplay from "./ErrorsDisplay";
+
+import { Transition } from '@headlessui/react'
+import { CheckCircleIcon } from '@heroicons/react/24/outline'
+import { XMarkIcon } from '@heroicons/react/20/solid'
 
 import UserContext from "../context/UserContext";
 
@@ -11,6 +15,7 @@ const UserSettings = () => {
   const [prevUser, setPrevUser] = useState(); // eslint-disable-line
   const navigate = useNavigate();
   const [newPasswordValue, setNewPasswordValue] = useState("");
+  const [success, setSuccess] = useState(false)
 
   // Get the user details
   useEffect(() => {
@@ -39,7 +44,7 @@ const UserSettings = () => {
       id: user.id, // Include the user's ID, Using the user state directly
     };
 
-    // Check if each field has been modified and add it to the updatedUser object
+    // Check if each field has been modified and update the updatedUser object
     if (firstName.current.value !== user.firstName) {
       updatedUser.firstName = firstName.current.value;
     }
@@ -69,6 +74,7 @@ const UserSettings = () => {
           ...prevUser,
           ...updatedUser,
         }));
+        setSuccess(true)
       } else if (response.status === 403) {
         navigate(`/forbidden`);
       } else if (response.status === 500) {
@@ -286,6 +292,55 @@ const UserSettings = () => {
           </div>
         </div>
       </div>
+
+
+    {/* Global notification live region, render this permanently at the end of the document */}
+    <div
+        aria-live="assertive"
+        className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+      >
+        <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
+          {/* Notification panel, dynamically insert this into the live region when it needs to be displayed */}
+          <Transition
+            show={success}
+            as={Fragment}
+            enter="transform ease-out duration-300 transition"
+            enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+            enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="p-4">
+                <div className="flex items-start">
+                  <div className="flex-shrink-0">
+                    <CheckCircleIcon className="h-6 w-6 text-green-400" aria-hidden="true" />
+                  </div>
+                  <div className="ml-3 w-0 flex-1 pt-0.5">
+                    <p className="text-sm font-medium text-gray-900">Successfully saved!</p>
+                    <p className="mt-1 text-sm text-gray-500">You account settings have been updated!.</p>
+                  </div>
+                  <div className="ml-4 flex flex-shrink-0">
+                    <button
+                      type="button"
+                      className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      onClick={() => {
+                        setSuccess(false)
+                      }}
+                    >
+                      <span className="sr-only">Close</span>
+                      <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+
+
     </>
   );
 };
