73e07382f034b70dc74a3d41b7d63d9c15b5a011
Hans Steffens <hans@steffens.co>
2024-03-26 12:00:56 -0500

    CreateInvoice Component mockup without notes field

--- client/src/components/pages/CreateInvoice.jsx
+++ client/src/components/pages/CreateInvoice.jsx
@@ -5,6 +5,7 @@ import { api } from "../../utils/apiHelper";
 import { Menu, Transition } from "@headlessui/react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { FormatNumber } from "../../utils";
 
 import {
   EllipsisVerticalIcon,
@@ -60,7 +61,7 @@ const CreateInvoice = () => {
   // Define state for subtotal, total with tax, and amount due
   const [subtotal, setSubtotal] = useState(0);
   const [total, setTotal] = useState(0);
-  const [amountDue, setAmountDue] = useState(0);
+  //const [amountDue, setAmountDue] = useState(0);
   const [paid, setPaid] = useState(0);
 
   // Calculate subtotal, total with tax, and amount due
@@ -76,7 +77,7 @@ const CreateInvoice = () => {
 
     let newTotal = newSubtotal + taxValue;
     let newPaid = 0; // Initially, paid is 0
-    let newAmountDue = newTotal - paid; // Initially, amount due is the same as total
+    // let newAmountDue = newTotal - paid; // Initially, amount due is the same as total
     const userFrom = authUser
       ? `${authUser.firstName} ${authUser.lastName}`
       : "";
@@ -87,7 +88,7 @@ const CreateInvoice = () => {
     setSubtotal(newSubtotal);
     setTotal(newTotal);
     setPaid(newPaid);
-    setAmountDue(newAmountDue);
+    //setAmountDue(newAmountDue);
   }, [itemFields, taxRate, paid, authUser]);
 
   const handleItemChange = (index, field, value) => {
@@ -502,7 +503,10 @@ const CreateInvoice = () => {
                             </div>
                           </td>
                           <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            {item.amount ? `$${item.amount}` : "$0.00"}
+                            $
+                            <FormatNumber
+                              number={item.amount ? `${item.amount}` : "0.00"}
+                            />
                           </td>
                           <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                             {index > 0 && (
@@ -527,6 +531,7 @@ const CreateInvoice = () => {
               </div>
             </div>
 
+            {/* Add a new item */}
             <div className="relative mb-2">
               <div
                 className="absolute inset-0 flex items-center"
@@ -545,32 +550,99 @@ const CreateInvoice = () => {
                 </span>
               </div>
             </div>
+            {/*end of add a new item */}
+            <div className="mx-auto max-w-7xl pt-8 lg:flex lg:gap-x-16">
+              <aside className="flex overflow-x-auto border-b border-gray-900/5 lg:block lg:w-96 lg:flex-none lg:border-0">
+                <button
+                  type="button"
+                  onClick={handleAddItem}
+                  className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                >
+                  Add Item
+                </button>
+              </aside>
 
+              <div className="px-4 sm:px-6 lg:flex-auto lg:px-0">
+                <table className="flex justify-end divide-y divide-gray-300">
+                  <tbody className="divide-y divide-gray-200">
+                    <tr className="lg:border-t lg:border-gray-200">
+                      <th
+                        scope="row"
+                        className="px-0 py-4 font-normal text-gray-700 sm:hidden"
+                      >
+                        Subtotal
+                      </th>
+                      <th
+                        scope="row"
+                        colSpan={3}
+                        className="hidden px-0 py-4 text-left font-normal text-gray-700 sm:table-cell"
+                      >
+                        Subtotal
+                      </th>
+                      <td className="py-4 pl-8 pr-0 text-left tabular-nums text-gray-900">
+                        $<FormatNumber number={subtotal} />
+                      </td>
+                    </tr>
+                    <tr>
+                      <th
+                        scope="row"
+                        className="py-4 font-normal text-gray-700 sm:hidden"
+                      >
+                        Tax
+                      </th>
+                      <th
+                        scope="row"
+                        colSpan={3}
+                        className="hidden py-4 text-left font-normal text-gray-700 sm:table-cell"
+                      >
+                        Tax
+                      </th>
+                      <td className="pl-8 pr-0 py-4 text-left tabular-nums text-gray-900">
+                        <input
+                          type="text"
+                          inputMode="numeric"
+                          value={taxRate || ""}
+                          onChange={(e) =>
+                            setTaxRate(parseFloat(e.target.value))
+                          }
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          placeholder="Enter tax %"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <th
+                        scope="row"
+                        className="py-4 font-semibold text-gray-900 sm:hidden"
+                      >
+                        Grand Total
+                      </th>
+                      <th
+                        scope="row"
+                        colSpan={3}
+                        className="hidden py-4 text-left font-semibold text-gray-900 sm:table-cell"
+                      >
+                        Grand Total
+                      </th>
+                      <td className="py-4 pl-8 pr-0 text-left font-semibold tabular-nums text-gray-900">
+                        $<FormatNumber number={total.toFixed(2)} />
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                {/* Button to submit form */}
+              </div>
+            </div>
+            <div className="flex justify-end mt-16 pt-8 lg:border-t lg:border-gray-900/10">
             <button
-              type="button"
-              onClick={handleAddItem}
-              className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-            >
-              Add Item
-            </button>
-            <br />
-            <input
-              type="number"
-              value={taxRate || ""}
-              onChange={(e) => setTaxRate(parseFloat(e.target.value))}
-              placeholder="Tax Rate (%)"
-            />
-
-            {/* Totals */}
-            <p>SubTotal: {subtotal.toFixed(2)}</p>
-            <p>Total: {total.toFixed(2)}</p>
-            <p>Paid: {paid.toFixed(2)}</p>
-            <p>Amount Due: {amountDue.toFixed(2)}</p>
-
-            {/* Button to submit form */}
-            <button type="submit" disabled={loading}>
-              {loading ? "Creating..." : "Create Invoice"}
-            </button>
+                  type="submit"
+                  disabled={loading}
+                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  {loading ? "Creating..." : "Create Invoice"}
+                </button>
+            </div>
           </form>
         </div>
       </div>
